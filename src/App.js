5e4851379d5c3a17cb1e3bbd045d6c1185c30f55
LEE KYUNGIL <leeki19@koreaap.com>
2020-10-27 15:57:31 +0900

    Create button to change chart's xAxes arange

--- src/App.js
+++ src/App.js
@@ -1,98 +1,85 @@
-import React, { useEffect } from "react";
-import Chart from "chart.js";
-import product from "./musinsa-scouter-102618-export2.json";
-function App() {
-    useEffect(() => {
-        const ctx = document.getElementById("myChart");
-        let pLabels = [];
-        let pData = [];
-        let year, month, day, newDate;
-        const timeFormat = "MM/DD/YYYY HH:mm";
-        for (const [key, value] of Object.entries(product.calendar)) {
-            year = Number(key.substring(0, 4));
-            month = Number(key.substring(4, 6));
-            day = Number(key.substring(6, 8));
-            newDate = new Date(year, month, day);
+import React, { useEffect, useState } from 'react';
+import Chart from 'chart.js';
+import product from './musinsa-scouter-102618-export2.json';
 
-            pLabels.push(newDate);
-            pData.push({ x: newDate, y: Number(value.price) });
-        }
-        new Chart(ctx, {
-            type: "line",
+function getDate(dateString) {
+    const year = Number(dateString.substring(0, 4));
+    const month = Number(dateString.substring(4, 6));
+    const day = Number(dateString.substring(6, 8));
+    return new Date(year, month, day);
+}
+function App() {
+    const [size, setSize] = useState(7);
+    useEffect(() => {
+        const ctx = document.getElementById('myChart');
+        const days = Object.entries(product.calendar);
+        const pLabels = days.map(([key]) => key);
+        const pData = days.map(([key, { price }]) => ({
+            x: getDate(key),
+            y: Number(price),
+        }));
+        const chart_ = new Chart(ctx, {
+            type: 'line',
             data: {
-                labels: pLabels,
+                labels: pLabels.slice(-1 * size, -1),
+
                 datasets: [
                     {
-                        label: "price",
-                        data: pData,
+                        label: 'price',
+                        data: pData.slice(-1 * size, -1),
 
                         // Point Styling
                         pointBorderWidth: (context) => 3,
-                        pointBorderColor: (context) => "rgba(0, 0, 0, 0.3)",
-                        pointBackgroundColor: (context) => "rgba(0, 0, 0, 0.3)",
-                        pointHoverBackgroundColor: (context) => "rgba(0, 0, 0, 1)",
-                        pointHoverBorderColor: (context) => "rgba(0, 0, 0, 1)",
+                        pointBorderColor: (context) => 'rgba(0, 0, 0, 0.3)',
+                        pointBackgroundColor: (context) => 'rgba(0, 0, 0, 0.3)',
+                        pointHoverBackgroundColor: (context) => 'rgba(0, 0, 0, 1)',
+                        pointHoverBorderColor: (context) => 'rgba(0, 0, 0, 1)',
 
                         // Line Styling
                         borderWidth: (context) => 1,
-                        borderColor: (context) => "rgba(0, 0, 0, 0.9)",
-                        backgroundColor: (context) => "rgba(0, 0, 0, 0.01)",
+                        borderColor: (context) => 'rgba(0, 0, 0, 0.9)',
+                        backgroundColor: (context) => 'rgba(0, 0, 0, 0.01)',
 
                         // fill: (context) => false,
-                        hoverBackgroundColor: (context) => "rgba(0, 0, 0, 0.1)",
-
-                        // General
-                        // xAxisID: "date-x-axis",
-                        // yAxisID: "price-y-axis",
+                        hoverBackgroundColor: (context) => 'rgba(0, 0, 0, 0.1)',
                     },
                 ],
             },
             options: {
                 tooltips: {
-                    mode: "index",
+                    mode: 'index',
                     intersect: false,
                 },
                 title: {
-                    text: "product price",
+                    text: 'product price',
                 },
                 scales: {
                     xAxes: [
                         {
-                            id: "date-x-axis",
-                            type: "time",
-                            time: {
-                                parser: timeFormat,
-                                tooltipFormat: "YYYY.MM.DD",
-                            },
+                            display: true,
                             scaleLabel: {
                                 display: true,
-                                labelString: "Date",
-                            },
-                            ticks: {
-                                major: {
-                                    fontStyle: "bold",
-                                    fontColor: "#FF0000",
-                                },
+                                labelString: '날짜',
                             },
                         },
                     ],
                     yAxes: [
                         {
-                            id: "price-y-axis",
-                            // type: "time",
-                            ticks: {
-                                beginAtZero: true,
-                            },
+                            display: true,
                         },
                     ],
                 },
             },
         });
-    }, []);
+        return () => chart_.destroy();
+    }, [size]);
 
     return (
-        <div className="App">
-            <canvas id="myChart"></canvas>
+        <div className='App'>
+            <button onClick={() => setSize(4)}>3일</button>
+            <button onClick={() => setSize(8)}>7일</button>
+            <button onClick={() => setSize(31)}>30일</button>
+            <canvas id='myChart'></canvas>
         </div>
     );
 }
